8d20cf41da426740c5bf16ecbd300d47255fa88e
felypen <146992891+felypen@users.noreply.github.com>
2025-06-09 08:20:11 -0300

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".parametro-senha_botao");
+const numeroSenha = document.querySelector(".parametro-snha_texto");
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelector(".parametro-senha_botao");
+
+console.log 
