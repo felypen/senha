d70b95bea8889a6b1f32921f34664975d3528a1b
felypen <146992891+felypen@users.noreply.github.com>
2025-06-09 08:30:03 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 document.querySelector(".parametro-senha_botao");
 const numeroSenha = document.querySelector(".parametro-snha_texto");
-numeroSenha.textContent = 5;
+
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelector(".parametro-senha_botao");
+botoes[0].onclick = diminuirTamanho;
+botoes[0].onclick = aumentarTamanho;
 
 console.log(botoes);
+
+function diminuirTamanho(){
+tamanhoSenha = tamanhoSenha - 1;
+numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentarTamanho(){
+tamanhoSenha = tamanhoSenha - 1;
+numeroSenha.textContent = tamanhoSenha
+}
